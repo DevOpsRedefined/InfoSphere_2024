8ad1cb0c0743a70595c2da83310e334330d36655
MeetAmin <86077076+MeetAmin@users.noreply.github.com>
2024-08-18 19:51:14 -0600

    update enqueue_styles and scripts

--- wp-content/themes/DevOpsRedefined_InfoSphere/enqueue-assets/enqueue-block-styles.php
+++ wp-content/themes/DevOpsRedefined_InfoSphere/enqueue-assets/enqueue-block-styles.php
@@ -10,10 +10,12 @@
 
 function add_styles_block_core_navigation_link()
 {
-    $src_css = '/assets/block-customization/css/for-admin-editor/admin-editor-custom-navigation-link.css';
+    $core_navigation_lin_src_css = '/assets/block-customization/css/for-admin-editor/admin-editor-custom-navigation-link.css';
+    $custom_slider_partners_logos_css = '/assets/block-customization/css/for-admin-editor/admin-editor-custom-logo-slider.css';
 
     add_theme_support('editor-styles');
-    add_editor_style($src_css);
+    add_editor_style($core_navigation_lin_src_css);
+    add_editor_style($custom_slider_partners_logos_css);
 }
 
 add_action('after_setup_theme', 'add_styles_block_core_navigation_link');
--- wp-content/themes/DevOpsRedefined_InfoSphere/enqueue-assets/enqueue-blockFE-scripts.php
+++ wp-content/themes/DevOpsRedefined_InfoSphere/enqueue-assets/enqueue-blockFE-scripts.php
@@ -8,13 +8,13 @@ function custom_slick_slider_block_assets()
     if (is_home() || is_front_page()) {
         // 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js',
 
-        wp_enqueue_script(
-            'slick-slider',
-            'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js',
-            array('jquery'),
-            '1.8.1',
-            true
-        );
+        // wp_enqueue_script(
+        //     'slick-slider',
+        //     'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js',
+        //     array('jquery'),
+        //     '1.8.1',
+        //     true
+        // );
 
         // wp_enqueue_style(
         //     'slick-slider-css',
@@ -41,5 +41,22 @@ function custom_slick_slider_block_assets()
         // print('somethjing here' . $slider_src);
         wp_enqueue_script($FE_slider__handle, $FE_slider__src, $depts, $FE_slider__ver, $in_footer);
     }
+
+
+    // The core GSAP library
+    wp_enqueue_script('gsap-js', 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js', array(), false, true);
+    // ScrollTrigger - with gsap.js passed as a dependency
+    wp_enqueue_script('gsap-st', 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js', array('gsap-js'), false, true);
+    // wp_enqueue_script('scroll-smoother', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/ScrollSmoother.min.js', array('gsap'), '3.11.4', true);
+    wp_enqueue_script('lenis-scroll-smoother', 'https://unpkg.com/lenis@1.1.9/dist/lenis.min.js', array(), false, true);
+
+    // https://assets.codepen.io/16327/ScrollSmoother.min.js
+    // Your animation code file - with gsap.js passed as a dependency
+    wp_enqueue_script('gsap-js2', get_template_directory_uri() .  '/assets/FE-scripts/frontend-gsap.js', array('gsap-js'), false, true);
+
+
+    // wp_enqueue_script('gsap', get_template_directory_uri() . '/js/gsap/gsap.min.js', array(), '3.11.4', true);
+    // wp_enqueue_script('scroll-trigger', get_template_directory_uri() . '/js/gsap/ScrollTrigger.min.js', array('gsap'), '3.11.4', true);
+    // wp_enqueue_script('smooth-scroll', get_template_directory_uri() . '/js/smooth-scroll.js', array('gsap', 'scroll-trigger'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'custom_slick_slider_block_assets');
